a76e6ecebbfc0c78f97f771b84ad0de695240f4d
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2021-12-13 00:57:49 +0100

    jpa

--- src/main/java/pl/polsl/UnoApi/repository/GameRepository.java
+++ src/main/java/pl/polsl/UnoApi/repository/GameRepository.java
@@ -3,7 +3,10 @@ package pl.polsl.UnoApi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.polsl.UnoApi.repository.dao.Game;
 
+import java.util.List;
+
 public interface GameRepository extends JpaRepository<Game, Long> {
 
+    List<Game> findAllByGameStateIsBefore(String end);
 
 }

aaa5fe6fa285b233b3f694530a8e12d239f8c4bd
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2021-12-21 02:36:41 +0100

    Pom update

--- src/main/java/pl/polsl/UnoApi/UnoApiBE.java
+++ src/main/java/pl/polsl/UnoApi/UnoApiBE.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @EnableConfigurationProperties
 @SpringBootApplication
 public class UnoApiBE {
